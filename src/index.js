589bf657ad2522f632087cc40e677d576b352168
unknown <gushcha1128@gmail.com>
2023-02-13 12:50:43 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let result = true;
+    
+    const openBrck = bracketsConfig.flat().filter((brck, i) => i % 2 === 0);
+    const closeBrck = bracketsConfig.flat().filter((brck, i) => i % 2 === 1);
+    const checkArr = [];
+    const strToArray = str.split('');
+
+    if (strToArray.length % 2 != 0) {
+        result = false;
+    } else {
+        strToArray.forEach(elnt => {
+            if (closeBrck.includes(elnt)) {
+                if (openBrck.includes(elnt) && !checkArr.includes(elnt)) {
+                    checkArr.push(elnt)
+                } else {
+                    let lastElnt = checkArr.pop()
+                    if (closeBrck.indexOf(elnt) === openBrck.indexOf(lastElnt)) {
+                        result = true;
+                    } else {
+                        result = false;
+                    }
+                }
+            } else if (openBrck.includes(elnt)) {
+                checkArr.push(elnt)
+            }
+        });
+    }
+    if (checkArr.length > 0) {
+        result = false;
+    }
+    return result;
 }
+
